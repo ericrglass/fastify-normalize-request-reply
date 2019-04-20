9f2405e16c0dced314a4ae525e09dc0cc5cb2340
Eric R Glass <ericrglass@gmail.com>
2019-04-20 08:37:48 -0400

    Updated to normalize changes made to request and response in middleware

--- index.js
+++ index.js
@@ -3,16 +3,95 @@
 const fp = require('fastify-plugin')
 const normalizeRequest = require('./normalize-request')
 const normalizeReply = require('./normalize-reply')
-const createApp = require('./create-app');
+const createApp = require('./create-app')
 
 function normalizeRequestReplyPlugin(fastify, options, next) {
-  const app = createApp(options);
+  const app = createApp(options)
+
+  const normalize = (req, reply) => {
+    const rawParent = Object.getPrototypeOf(req.raw)
+    Object.getOwnPropertyNames(rawParent).forEach((name) => {
+        if (!req[name]) {
+            try {
+                req[name] = req.raw[name]
+            } catch (e) {}
+        }
+    })
+    Object.getOwnPropertyNames(req.raw).forEach((name) => {
+        if (!req[name]) {
+            try {
+                req[name] = req.raw[name]
+            } catch (e) {}
+        }
+    })
+    const reqParent = Object.getPrototypeOf(req)
+    Object.getOwnPropertyNames(reqParent).forEach((name) => {
+        if (!req.raw[name]) {
+            try {
+                req.raw[name] = req[name]
+            } catch (e) {}
+        }
+    })
+    Object.getOwnPropertyNames(req).forEach((name) => {
+        if (!req.raw[name]) {
+            try {
+                req.raw[name] = req[name]
+            } catch (e) {}
+        }
+    })
+    const resParent = Object.getPrototypeOf(reply.res)
+    Object.getOwnPropertyNames(resParent).forEach((name) => {
+        if (!reply[name]) {
+            try {
+                reply[name] = reply.res[name]
+            } catch (e) {}
+        }
+    })
+    Object.getOwnPropertyNames(reply.res).forEach((name) => {
+        if (!reply[name]) {
+            try {
+                reply[name] = reply.res[name]
+            } catch (e) {}
+        }
+    })
+    const replyParent = Object.getPrototypeOf(reply)
+    Object.getOwnPropertyNames(replyParent).forEach((name) => {
+        if (!reply.res[name]) {
+            try {
+                reply.res[name] = reply[name]
+            } catch (e) {}
+        }
+    })
+    Object.getOwnPropertyNames(reply).forEach((name) => {
+        if (!reply.res[name]) {
+            try {
+                reply.res[name] = reply[name]
+            } catch (e) {}
+        }
+    })
+  }
 
   fastify.addHook("onRequest", (req, reply, hookNext) => {
     normalizeRequest(req, reply, app)
     normalizeReply(reply, req, app)
     hookNext()
   })
+
+  fastify.addHook('preParsing', (req, reply, hookNext) => {
+    normalize(req, reply)
+    hookNext()
+  })
+
+  fastify.addHook('preValidation', (req, reply, hookNext) => {
+    normalize(req, reply)
+    hookNext()
+  })
+
+  fastify.addHook('preHandler', (req, reply, hookNext) => {
+    normalize(req, reply)
+    hookNext()
+  })
+
   next()
 }
 
